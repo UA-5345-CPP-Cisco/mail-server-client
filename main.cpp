763c80c16a59108df12ffc8008ddc1b11c528767
Roman Mykhailiv <rv140208@gmail.com>
2026-06-17 17:52:38 +0300

    Added thread pool logger

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include"Server.hpp"
+
+int main(void)
+{
+    auto& logger=Logger::GetInstance();
+    logger.Initialize("app.log", LogLevels::Trace, true);
+    const unsigned int MAX_THREADS=std::thread::hardware_concurrency();
+    Server server(MAX_THREADS);
+    std::cout<<"Server started\n";
+    server.StartServer();
+    return 0;
+    //./mail_server
+}
