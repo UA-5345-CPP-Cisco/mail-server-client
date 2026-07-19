9369029f1c17be68b3cc822400e89c3175d9687a
Yana <dekamics20@gmail.com>
2026-07-19 22:39:21 +0300

    refactor: update AuthHandlerTests to use correct class name and improve test cases

--- tests-client/model_tests/AuthHandlerTests.cpp
+++ tests-client/model_tests/AuthHandlerTests.cpp
@@ -7,36 +7,36 @@
 
 class AuthHandlerTests : public ::testing::Test {
     protected:
-    std::unique_ptr<AuthHandler> AuthHandler;
+    std::unique_ptr<ISXAuth::AuthHandler> auth_handler;
     std::unique_ptr<Storage::Database> db;
 
     void SetUp() override{
         ISXDatabaseManager::DatabaseManager::EnsureInitialized();
         this->db = std::make_unique<Storage::Database>(ISXDatabaseManager::DatabaseManager::DatabasePath());
         this->db->Execute("DELETE FROM users;");
-        this->AuthHandler = std::make_unique<AuthHandler>(*this->db);
+        this->auth_handler = std::make_unique<ISXAuth::AuthHandler>(*this->db);
     }
 
     void TearDown() override {
         this->db.reset();
-        this->AuthHandler.reset();
+        this->auth_handler.reset();
     }
 };
 
-TEST_F(AuthHandlerTests,WhenregisterUser_RegistrationHandlerDoesReturnFalse) {
+TEST_F(AuthHandlerTests,WhenregisterUser_AuthHandlerDoesReturnFalse) {
     const QString username="";
     const QString email="";
     const QString password="";
-    EXPECT_FALSE(AuthHandler->RegisterUser(username, "Some text", "Some text"));
-    EXPECT_FALSE(AuthHandler->RegisterUser("Some text", email, "Some text"));
-    EXPECT_FALSE(AuthHandler->RegisterUser("Some text", "Some text", password));
+    EXPECT_FALSE(auth_handler->RegisterUser(username, "Some text", "Some text"));
+    EXPECT_FALSE(auth_handler->RegisterUser("Some text", email, "Some text"));
+    EXPECT_FALSE(auth_handler->RegisterUser("Some text", "Some text", password));
 }
 
-TEST_F(AuthHandlerTests,WhenregisterUser_RegistrationHandlerDoesReturnTrue) {
+TEST_F(AuthHandlerTests,WhenregisterUser_AuthHandlerDoesReturnTrue) {
     const QString username="Bob";
     const QString email="bob@gmail";
     const QString password="Some text";
-    EXPECT_TRUE(this->AuthHandler->RegisterUser(username, email, password));
+    EXPECT_TRUE(this->auth_handler->RegisterUser(username, email, password));
     Storage::UserRepository repo(*this->db);
     auto user=repo.FindByEmail(email.toStdString());
     EXPECT_TRUE(user.has_value());
@@ -47,8 +47,8 @@ TEST_F(AuthHandlerTests, WhenRegisterDuplicateUser_ReturnsFalse) {
     const QString username = "bob";
     const QString email = "bob@gmail";
     const QString password = "bob_top";
-    EXPECT_TRUE(this->AuthHandler->RegisterUser(username, email, password));
-    EXPECT_FALSE(this->AuthHandler->RegisterUser("AnotherName", email, "newpassword"));
+    EXPECT_TRUE(this->auth_handler->RegisterUser(username, email, password));
+    EXPECT_FALSE(this->auth_handler->RegisterUser("AnotherName", email, "newpassword"));
 }
 
 
